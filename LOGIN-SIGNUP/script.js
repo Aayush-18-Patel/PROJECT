69478ea7a120c849db55dd62b0154e55f752c3ef
Ayush-18-Patel <ayushkpatel1801@gmail.com>
2025-02-03 16:57:46 +0530

    Initial commit

--- LOGIN-SIGNUP/script.js
+++ LOGIN-SIGNUP/script.js
@@ -22,7 +22,6 @@ document.addEventListener('DOMContentLoaded', () => {
         window.location.href = '../HOME/index.html';
     });
 };
-;;;;;;;;;;;;;;;;;;;
     signUpButton.addEventListener('click', () => container.classList.add('right-panel-active'));
     signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
 
